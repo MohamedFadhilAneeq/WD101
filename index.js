23d6a352f81e566c55459bfe73a2822fa8fee5cd
Mohamed Fadhil Aneeq H <155130448+MohamedFadhilAneeq@users.noreply.github.com>
2024-09-14 00:34:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const displayEntries = () => {
       return row;
     })
     .join("\n");
-  const table = `<table><tr><th>Name</th><th>Email</th><th>Password</th><th>dob</th><th>accepted terms?</th></tr>${tableEntries}</table>`;
+  const table = `<table><tr><th>Name</th><th>Email</th><th>Password</th><th>dob</th><th>Accepted terms?</th></tr>${tableEntries}</table>`;
 
   let details = document.getElementById("table");
   details.innerHTML = table;
